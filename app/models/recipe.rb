cfd23fbefa9618ca57a17acbe5fc51e89776ab8c
scobar11 <scottbaron1@msn.com>
2015-11-11 21:15:55 -0500

    end of class

---
+++ app/models/recipe.rb
@@ -0,0 +1,10 @@
+class Recipe < ActiveRecord::Base
+# def minutes_total
+#		if(minutes_to_prepare)
+#			minutes_to_prepare + minutes_to_cook
+#		else
+#			minutes_to_cook
+#		end
+#	end
+
+end
---
+++ db/migrate/20151112012010_create_recipes.rb
@@ -0,0 +1,12 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :category
+      t.text :ingredients
+      t.text :instructions
+
+      t.timestamps null: false
+    end
+  end
+end
